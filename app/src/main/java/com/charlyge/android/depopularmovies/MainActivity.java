21fbd31529c3c5d01b8413cdd910267a3e39c154
charlyge <charlyge2@gmail.com>
2018-07-27 22:07:21 +0100

    first commit

---
+++ app/src/main/java/com/charlyge/android/depopularmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.charlyge.android.depopularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
